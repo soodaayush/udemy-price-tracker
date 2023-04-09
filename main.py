d2a6216b55d8de2f1314b840713cb82de96b4aa8
DevBaddy <warbaddy19+github@gmail.com>
2023-04-09 18:15:37 -0300

    Working on storing data in a dictionary

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-account_sid = os.getenv("TWILIO_ACCOUNT_ID")
-auth_token = os.getenv("TWILIO_AUTH_TOKEN")
-client = Client(account_sid, auth_token)
-message = client.messages.create(
-    body="Hello from Twilio",
-    from_=os.getenv("TWILIO_PHONE_NUMBER"),
-    to=os.getenv("RECIPIENT_PHONE_NUMBER")
-)
-print(message.sid)
+
+# account_sid = os.getenv("TWILIO_ACCOUNT_ID")
+# auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+# client = Client(account_sid, auth_token)
+# message = client.messages.create(
+#     body=f"{arr}",
+#     from_=os.getenv("TWILIO_PHONE_NUMBER"),
+#     to=os.getenv("RECIPIENT_PHONE_NUMBER")
+# )
 
 
 async def main():
@@ -26,18 +26,39 @@ async def main():
 
     arr = []
 
+    courses = await url.querySelectorAll(".course-list--container--FuG0T")
+
+    print(courses)
+    #
+    # for course in courses:
+    #     element = await course.querySelector("h3")
+    #     print(await element.jsonValue())
+
     coursesInfo = await url.querySelectorAll("div .ud-sr-only > span")
     courseTitles = await url.querySelectorAll("div .course-card--has-price-text--1c0ze > h3 > a")
 
-    for courseTitle in courseTitles:
-        courseTitle = await courseTitle.getProperty("textContent")
+    # for courseTitle in courseTitles:
+    #     courseTitle = await courseTitle.getProperty("textContent")
+    #
+    #     print(await courseTitle.jsonValue())
 
-        print(await courseTitle.jsonValue())
+    coursesObj = {}
 
     for courseInfo in coursesInfo:
         courseInfo = await courseInfo.getProperty("textContent")
 
-        print(await courseInfo.jsonValue())
+        courseObj = {}
+
+        # print(type(await courseInfo.jsonValue()))
+
+        if "Original" in await courseInfo.jsonValue():
+            courseObj.update(originalPrice=await courseInfo.jsonValue())
+        elif "Current" in await courseInfo.jsonValue():
+            courseObj.update(currentPrice=await courseInfo.jsonValue())
+
+        arr.append(courseObj)
+
+    print(arr)
 
     # html = await url.content()
     await browserObj.close()
