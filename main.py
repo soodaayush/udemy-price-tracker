e702c94d0d76f6bc2926e7037ae207e24288d74a
DevBaddy <warbaddy19+github@gmail.com>
2023-04-13 18:08:31 -0300

    Added code that stores links, working on adding titles

--- main.py
+++ main.py
@@ -24,32 +24,35 @@ async def main():
     await url.goto('http://127.0.0.1:5500/index.html')
     await url.waitFor(3000)
 
-    arr = []
-    titles = []
+    i = 0
+    courses = []
+    links = []
+    courseObj = {}
 
     coursesInfo = await url.querySelectorAll("div .ud-sr-only > span")
-    courseTitles = await url.querySelectorAll("div .course-card--has-price-text--1c0ze > h3 > a")
-
-    courseObj = {}
+    courseLinks = await url.querySelectorAll("div .course-card--has-price-text--1c0ze > h3 > a")
+    courseTitles = await url.querySelectorAll("div .course-card--has-price-text--1c0ze > h3")
 
     for courseTitle in courseTitles:
         courseTitle = await courseTitle.getProperty("textContent")
 
-        if "<strong>" not in await courseTitle.jsonValue() and ":" not in await courseTitle.jsonValue():
-            titles.append(await courseTitle.jsonValue())
+        stringified = str(await courseTitle.jsonValue())
 
-        print(await courseTitle.jsonValue())
+        stringified = stringified.splitlines()
+
+        print(stringified)
+
+        print(f"{await courseTitle.jsonValue()} End line")
+
+    for courseLink in courseLinks:
+        courseLink = await courseLink.getProperty("href")
+
+        links.append(await courseLink.jsonValue())
 
     for courseInfo in coursesInfo:
-        i = 0
-
         courseInfo = await courseInfo.getProperty("textContent")
 
-        # print(await courseInfo.jsonValue())
-
-        title = titles[i]
-
-        courseObj.update(title=titles[i])
+        courseObj.update(url=links[i])
 
         if "Original" in await courseInfo.jsonValue():
             courseObj.update(originalPrice=await courseInfo.jsonValue())
@@ -57,11 +60,11 @@ async def main():
             courseObj.update(currentPrice=await courseInfo.jsonValue())
 
         if "originalPrice" in courseObj and "currentPrice" in courseObj:
-            arr.append(courseObj)
+            courses.append(courseObj)
             courseObj = {}
             i = i + 1
 
-    print(arr)
+    print(courses)
 
     # html = await url.content()
     await browserObj.close()
